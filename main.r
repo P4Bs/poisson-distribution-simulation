911ee201df01f71f7843b87ecb9b97049a70ab0e
Pablo Moreno <PabloJMP@uma.es>
2022-05-18 20:58:21 +0200

    Le cambie el color y otras cositas :>

--- main.r
+++ main.r
@@ -1,15 +1,15 @@
-## TRABAJITO TO CHINGON DE R - PABLO MORENO Y JUAN MARQUÉS GARRIDO :>
+## TRABAJITO TO CHINGON DE R - PABLO MORENO Y JUAN MARQUES GARRIDO :>
 
 ## ACERCA: HEMOS DECIDIDO HACER NUESTRO TRABAJO CON LA DISTRIBUCION DE POISSON PARA ASI
 ## OBTENER LA MAYOR NOTA POSIBLE :D
 
 
-## EJERCICIO Nº 1 - GENERACION DE UNA MUESTRA DE TAMAÑO N
+## EJERCICIO 1 - GENERACION DE UNA MUESTRA DE TAMANYO N
 
-# VAMOS A TRABAJAR CON UNA MUESTRA DE TAMAÑO N = 150
+# VAMOS A TRABAJAR CON UNA MUESTRA DE TAMANYO N = 150
 
 ### PARAMETROS ###
-sample_size <- 20000
+sample_size <- 100000
 poisson_parameter <- 20 ## PROVISIONAL !!!!
 pois_values <- vector()
 
@@ -24,12 +24,12 @@ gen_value_poisson <- function(pois_lambda) {
   return(count)
 }
 
-## GENERAMOS NUESTROS VALORES ##
+## GENERANDO NUESTOR VECTOR DE VALORES ##
 n_iter <- 0;
 repeat{
   pois_values <- c(pois_values, gen_value_poisson(poisson_parameter))
   n_iter <- n_iter + 1
-  
+
   if (n_iter >= sample_size) {
     break
   }
@@ -37,10 +37,10 @@ repeat{
 
 ## GENERAMOS NUESTRO HISTOGRAMA PARA LA DISTRIBUCION DE FRECUENCIAS ##
 hist(pois_values,
-     main=paste("Poisson Distribution Simulation with",sample_size, 
-                "values and Î»=", poisson_parameter),
-     xlab="Generated Values",
-     xlim=c(0,poisson_parameter*2),
-     breaks=poisson_parameter*2,
-     col='pink'
+     main = paste("Poisson Distribution Simulation", sample_size,
+                  "values, lambda =", poisson_parameter),
+     xlab = "Generated Values",
+     xlim = c(0, poisson_parameter * 2),
+     breaks = poisson_parameter * 2,
+     col = "#79cc5b"
      )
