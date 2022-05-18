533792ac246988503c29dee1a425d40402a43b1b
Pablo Moreno <PabloJMP@uma.es>
2022-05-18 22:28:14 +0200

    Optimizacion Codigo y cambios en el Markdown :>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # trabajo-poisson
-Un repositorio para el trabajo de estadistica sobre la simulación de la distribución de Poisson :>
+## Integrantes
+- Juan Marqués Garrido 
+<img src="https://emoji.gg/assets/emoji/4610_Peepo_happy.png" width="30"/>
+- Pablo Jesús Moreno Polo
+<img src="https://emoji.gg/assets/emoji/4610_Peepo_happy.png" width="30"/>
+---
+## Proposito
+The purpose of this repository is to host the code of our Statistics Simulation written in the R language.
+
+<p align = "center">
+    <img src = "https://sonuprabhu.files.wordpress.com/2016/09/logo-r.png">
+</p>
--- main.r
+++ main.r
@@ -1,46 +1,37 @@
-## TRABAJITO TO CHINGON DE R - PABLO MORENO Y JUAN MARQUES GARRIDO :>
-
-## ACERCA: HEMOS DECIDIDO HACER NUESTRO TRABAJO CON LA DISTRIBUCION DE POISSON PARA ASI
-## OBTENER LA MAYOR NOTA POSIBLE :D
-
-
 ## EJERCICIO 1 - GENERACION DE UNA MUESTRA DE TAMANYO N
 
-# VAMOS A TRABAJAR CON UNA MUESTRA DE TAMANYO N = 150
+### VARIABLES ###
+sample_size <- 10000
+lambda <- 2 ## PROVISIONAL !!!!
+simulation_values <- vector()
 
-### PARAMETROS ###
-sample_size <- 100000
-poisson_parameter <- 20 ## PROVISIONAL !!!!
-pois_values <- vector()
-
-## FUNCION PARA GENERAR NUESTROS VALORES DE POISSON ##
-gen_value_poisson <- function(pois_lambda) {
+## FUNCION PARA GENERAR NUESTROS VALORES DE LA DISTRIBUCION POISSON ##
+gen_poisson_values <- function(lambda) {
   sum <- 0
   count <- 0
-  while (sum < pois_lambda) {
-    sum <- sum + -1 * log(runif(1))
+  while (sum < lambda) {
+    sum <- sum + -log(runif(1))
     count <- count + 1
   }
   return(count)
 }
 
 ## GENERANDO NUESTOR VECTOR DE VALORES ##
-n_iter <- 0;
-repeat{
-  pois_values <- c(pois_values, gen_value_poisson(poisson_parameter))
-  n_iter <- n_iter + 1
+for (i in 1:sample_size) simulation_values[i] <- gen_poisson_values(lambda)
+max_value <- simulation_values[which.max(simulation_values)]
+min_value <- simulation_values[which.min(simulation_values)]
 
-  if (n_iter >= sample_size) {
-    break
-  }
-}
 
+## EJERCICIO 2 - HISTOGRAMA DE LA DISTRIBUCION DE FRECUENCIAS ##
 ## GENERAMOS NUESTRO HISTOGRAMA PARA LA DISTRIBUCION DE FRECUENCIAS ##
-hist(pois_values,
+hist(simulation_values,
      main = paste("Poisson Distribution Simulation", sample_size,
-                  "values, lambda =", poisson_parameter),
+                  "values, lambda =", lambda),
      xlab = "Generated Values",
-     xlim = c(0, poisson_parameter * 2),
-     breaks = poisson_parameter * 2,
+     xlim = c(min_value, max_value),
+     breaks = max_value,
      col = "#79cc5b"
      )
+
+
+## EJERCICIO 3 - ESTIMACION DE PROBABILIDADES DE INTERVALOS ##
