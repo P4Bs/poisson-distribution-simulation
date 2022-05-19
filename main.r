ca72a7f81b3fd0449c1be0d176a8db803810e0f0
Pablo Moreno <PabloJMP@uma.es>
2022-05-19 12:27:41 +0200

    Somos unos makinas 😎

--- main.r
+++ main.r
@@ -2,7 +2,7 @@
 
 ### VARIABLES ###
 sample_size <- 100000
-lambda <- 19 ## PROVISIONAL !!!!
+lambda <- 109 ## PROVISIONAL !!!!
 simulation_values <- integer()
 
 ## FUNCION PARA GENERAR NUESTROS VALORES DE LA DISTRIBUCION POISSON ##
@@ -18,18 +18,48 @@ gen_poisson_values <- function(lambda) {
 
 ## GENERANDO NUESTOR VECTOR DE VALORES ##
 for (i in 1:sample_size) simulation_values[i] <- gen_poisson_values(lambda)
-
-frequency <- as.data.frame(table(simulation_values))
-min_val
-max_val <- frequency$simulation_values[nrow(frequency)]
-
-
+min_val <- min(simulation_values)
+max_val <- max(simulation_values)
 
 ## EJERCICIO 2 - HIS  TOGRAMA DE LA DISTRIBUCION DE FRECUENCIAS ##
 ## GENERAMOS NUESTRO HISTOGRAMA PARA LA DISTRIBUCION DE FRECUENCIAS ##
 hist(simulation_values,
-     xlab = "Generated Values",
-     col = "#79cc5b"
+      main = paste("Poisson Distribution Simulation", sample_size,
+                    "values, lambda =", lambda),
+      xlab = "Generated Values",
+      xlim = c(min_val, max_val),
+      breaks = max_val - min_val,
+      col = "#79cc5b"
      )
 
 ## EJERCICIO 3 - ESTIMACION DE PROBABILIDADES DE INTERVALOS ##
+
+
+## EJERCICIO 4 - VALORES PUNTUALES DE LA MEDIA Y LA VARIANZA MUESTRAL 
+sample_mean <- mean(simulation_values)
+sample_var <- var(simulation_values)
+
+## EJERICICIO 5 - APLICACION DEL TEOREMA CENTRAL DEL LIMITE 
+
+clt_function <- function(x) {
+  num_aux <- (x - sample_mean) / sqrt(sample_var)
+  return(num_aux)
+}
+
+clt_values <- sapply(simulation_values, clt_function)
+clt_mean <- mean(clt_values)
+clt_var <- var(clt_values)
+
+
+hist(clt_values,
+     xlab = "Generated Values",
+     col = "#79cc5b"
+)
+
+sample_normal <- rnorm(100000)
+hist(sample_normal,
+      xlab = "Generated Values",
+      col = "pink"
+)
+
+par(mfrow = c(1, 2)) #cambiar a (1,2) para sacar la superposicion de graficas
